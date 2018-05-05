352b300c008463d1e682a1062f904ee10a7a10bf
Spyros Argyroiliopoulos <spyros.argyroiliopoulos@camelotglobal.com>
2018-05-05 04:14:06 +0300

    Initial commit, as is at branch 8_1-Primary-Bean-Annotation

---
+++ src/main/java/com/codehub/spring/core/controller/SetterInjectedController.java
@@ -0,0 +1,23 @@
+package com.codehub.spring.core.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.codehub.spring.core.service.GreetingService;
+
+
+//This annotation is needed to tell spring that this is a bean
+@Controller
+public class SetterInjectedController {
+    private GreetingService greetingService;
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+
+    @Autowired
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+        System.out.println("Using Setter");
+    }
+}
---
+++ src/main/java/com/codehub/spring/core/service/GreetingService.java
@@ -0,0 +1,6 @@
+package com.codehub.spring.core.service;
+
+public interface GreetingService {
+
+    String sayGreeting();
+}
