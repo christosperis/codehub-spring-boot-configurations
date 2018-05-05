5eb60da9db49c287beee601d5715cd6386f973bf
Spyros Argyroiliopoulos <spyros.argyroiliopoulos@camelotglobal.com>
2018-05-05 05:22:49 +0300

    Set, List, Map of Beans

--- src/main/java/com/codehub/spring/core/controller/AutowiredInjectedController.java
+++ src/main/java/com/codehub/spring/core/controller/AutowiredInjectedController.java
@@ -1,7 +1,10 @@
 package com.codehub.spring.core.controller;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import com.codehub.spring.core.service.GreetingService;
@@ -12,11 +15,19 @@ public class AutowiredInjectedController {
 
     //We need this annotation here to know where to Autowire
     @Autowired
-    @Qualifier("greetingServiceSpainImpl")
-    private GreetingService greetingService;
+    private List<GreetingService> greetingServiceList;
+
+    @Autowired
+    private Set<GreetingService> greetingServiceSet;
+
+    @Autowired
+    private Map<String, GreetingService> greetingServiceMap;
 
     public String sayHello() {
-        return greetingService.sayGreeting();
+        greetingServiceList.forEach(service -> System.out.println(service.getClass().getName()));
+        greetingServiceSet.forEach(service -> System.out.println(service.getClass().getName()));
+        greetingServiceMap.keySet().forEach(s -> System.out.println(s));
+        return greetingServiceList.get(0).sayGreeting();
     }
 
 }
